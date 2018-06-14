0a76589a7a614274fd847850447986647e5e90c8
etienne0n <etienne.onasch@haw-hamburg.de>
2018-06-14 12:19:09 +0200

    Kommentare hinzu

--- src/main/Coordinate.java
+++ src/main/Coordinate.java
@@ -8,12 +8,9 @@ package main;
 public class Coordinate {
 
 	/**
-	 * Latitude measures how far north or south of the equator a place is located.
-	 * The equator is situated at 0°, the North Pole at 90° north (or 90°, because a
-	 * positive latitude implies north), and the South Pole at 90° south (or –90°).
-	 * Latitude measurements range from 0° to (+/–)90°.
-	 * 
-	 * Deutsch: Laengengrad
+	 * Longitude measures how far east or west of the prime meridian a place is
+	 * located. The prime meridian runs through Greenwich, England. Longitude
+	 * measurements range from 0° to (+/–)180°.
 	 */
 	private final double longnitude;
 
@@ -23,10 +20,10 @@ public class Coordinate {
 	 * positive latitude implies north), and the South Pole at 90° south (or –90°).
 	 * Latitude measurements range from 0° to (+/–)90°.
 	 * 
-	 * Deutsch: Breitengrad
+	 * 
 	 */
 	private final double latitude;
-	
+
 	public Coordinate(double longnitude, double latitude) {
 		this.latitude = latitude;
 		this.longnitude = longnitude;
@@ -39,10 +36,5 @@ public class Coordinate {
 	public double getLatitude() {
 		return latitude;
 	}
-	
-	
-	
-	
-	
 
 }
