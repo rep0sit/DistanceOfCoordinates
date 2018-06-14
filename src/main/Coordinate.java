de56b070cc0c8b2bdc06768e1e09c9943d24b9d5
etienne0n <etienne.onasch@haw-hamburg.de>
2018-06-14 13:13:40 +0200

    Programm fertig und positiv getestet

--- src/main/Coordinate.java
+++ src/main/Coordinate.java
@@ -2,17 +2,35 @@ package main;
 
 /**
  * 
+ * PRECONDITION:<br><br>
+ * First Value is latitude:<br> 
+ * must be >= -90 and <= 90 <br><br>
+ * Second Value is longnitude:<br>
+ * must be >= -180 and <= 180 <br><br>
+ * 
+ * Every entry out of this bounds will be normalized:<br><br>
+ * For example the pair of values (-91, 181) will be normalized to (-90,180)<br>
+ * the pair of values (91, -181) will be normalized to (90, -180)
+ * 
+ * 
+ * 
  * @author Etienne Onasch, Nelli Welker
- *
+ * 
+ * 
  */
 public class Coordinate {
-
-	/**
-	 * Longitude measures how far east or west of the prime meridian a place is
-	 * located. The prime meridian runs through Greenwich, England. Longitude
-	 * measurements range from 0° to (+/–)180°.
-	 */
-	private final double longnitude;
+	
+	// CONSTANTS
+	
+	private static final double RADIUS_EARTH = 6371.0;
+	private static final double KM_PER_NM = 1.852;
+	
+	private static final double MIN_LAT = -90.0;
+	private static final double MAX_LAT = 90.0;
+	
+	private static final double MIN_LON = -180.0;
+	private static final double MAX_LON = 180.0;
+	
 
 	/**
 	 * Latitude measures how far north or south of the equator a place is located.
@@ -23,16 +41,76 @@ public class Coordinate {
 	 * 
 	 */
 	private final double latitude;
+	
+	/**
+	 * Longitude measures how far east or west of the prime meridian a place is
+	 * located. The prime meridian runs through Greenwich, England. Longitude
+	 * measurements range from 0° to (+/–)180°.
+	 */
+	private final double longnitude;
 
-	public Coordinate(double longnitude, double latitude) {
-		this.latitude = latitude;
-		this.longnitude = longnitude;
+	public Coordinate(double latitude, double longnitude) {
+		this.latitude = latitude < MIN_LAT ? MIN_LAT : latitude > MAX_LAT ? MAX_LAT : latitude;
+		this.longnitude = longnitude < MIN_LON ? MIN_LON : longnitude > MAX_LON ? MAX_LON : longnitude;
 	}
 
+	/**
+	 * Entfernung (in km) zwischen zwei Punkten auf der Erdoberfläche:<br><br>
+	 * 
+	 * Entf_km = 6371 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1))
+	 * 
+	 * Die Gleichung liefert die Länge des Großkeisbogens zwischen zwei Punkten
+	 * (lat1, lon1) und (lat2, lon2) auf einer Kugel mit einem Radius von 6371
+	 * Kilometern. Da die Erde keine perfekte Kugel ist (6371 km ist der mittlere
+	 * Radius), stellt die Berechnung eine Näherung dar, die vor allem für größere
+	 * Distanzen geeignet ist. Möchte man die Seemeile (= 1,852 km) als
+	 * fundamentales Abstandsmaß für das Geosystem zugrundelegen (der geliefert Wert
+	 * ist nach wie vor km, aber der Erdumfang wird als das 60*360-fache einer
+	 * Seemeile definiert), ersetzt man 6371 durch
+	 * 
+	 * 1.852 * 60 * 180/PI
+	 * 
+	 * Die Formel geht davon aus, dass die trigonometrischen Funktionen acos(),
+	 * sin(), cos() im Bogenmaß (rad) rechnen, was für Programmiersprachen
+	 * typischerweise der Fall ist. Liegen lat1, lon1, lat2, lon2 in Grad vor, was
+	 * bei Geopositionen üblich ist, müssen diese vor der Einsetzung ins Bogenmaß
+	 * umgerechnet, also mit Pi/180 multipliziert werden.
+	 * 
+	 * rad = degree * PI / 180
+	 * 
+	 * 
+	 * @param other die Andere Koordinate
+	 * @return die Entfernung zwischen beiden Koordinaten in KM
+	 */
+	public double getDistanceInKM(Coordinate other) {
+		
+		double lon1 = degreeToRad(longnitude);
+		double lat1 = degreeToRad(latitude);
+		
+		double lon2 = degreeToRad(other.getLongnitude());
+		double lat2 = degreeToRad(other.getLatitude());
+
+		return RADIUS_EARTH * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
+	}
+	/**
+	 * Rechnet Grad in rad um.
+	 * @param degree die Gradzahl
+	 * @return die Gradzahl umgerechnet in rad
+	 */
+	private double degreeToRad(double degree) {
+		return degree * Math.PI / 180.0;
+	}
+	/**
+	 * Gibt den Laengengrade dieser Koordinate zurueck
+	 * @return longnitude bzw. Laengengrad
+	 */
 	public double getLongnitude() {
 		return longnitude;
 	}
-
+	/**
+	 * Gibt den Breitengrad dieser Koordinate zurueck
+	 * @return latitude bzw. Breitengrad
+	 */
 	public double getLatitude() {
 		return latitude;
 	}
---
+++ test/main/CoordinateTest.java
@@ -0,0 +1,47 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoordinateTest {
+
+	@Test
+	public void testGetDistanceInKM() {
+		
+		// FAR DISTANCE 1
+		
+		Coordinate c1 = new Coordinate(-25.0, 160.0);
+		Coordinate c2 = new Coordinate(25.0, -160.0);
+
+		// Distance according to
+		// https://gps-coordinates.org/distance-between-coordinates.php
+		// = 7029.37 KM
+		
+		double dist = 7029.37;
+		//System.out.println(c1.getDistanceInKM(c2));
+		
+		double calculated = c1.getDistanceInKM(c2);
+		// Weniger als 1 % Abweichung
+		assertTrue(calculated > (dist * 0.99) && calculated < (dist * 1.01));
+		
+		// FAR DISTANCE 2
+		
+		c1 = new Coordinate(-90.0, 0.0);
+		c2 = new Coordinate(90.0, 0.0);
+		calculated = c1.getDistanceInKM(c2);
+		dist = 20015.09;
+		// Weniger als 1 % Abweichung
+		assertTrue(calculated > (dist * 0.99) && calculated < (dist * 1.01));
+		
+		// Close Distance 1
+		c1 = new Coordinate(33.01, 120.0);
+		c2 = new Coordinate(33.02, 120.0);
+		calculated = c1.getDistanceInKM(c2);
+		dist = 1.11;
+		// Weniger als 1 % Abweichung
+		assertTrue(calculated > (dist * 0.99) && calculated < (dist * 1.01));
+
+	}
+
+}
